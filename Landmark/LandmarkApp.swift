a3017c78931004f245c71de8923f106c7c216696
DanyST <lherreralillo@gmail.com>
2021-05-04 23:33:04 -0400

    Initial Commit

---
+++ Landmark/LandmarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarkApp.swift
+//  Landmark
+//
+//  Created by Luis Herrera Lillo on 04-05-21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
