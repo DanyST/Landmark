50d7627ca533518a38f4c983f67df6829505d3de
DanyST <lherreralillo@gmail.com>
2021-05-13 18:36:24 -0400

    Add LandmarkList View and conform Landmark model to Identifiable protocol

--- Landmark/Domain/Landmark.swift
+++ Landmark/Domain/Landmark.swift
@@ -9,7 +9,7 @@ import Foundation
 import SwiftUI
 import CoreLocation
 
-struct Landmark: Hashable, Codable {
+struct Landmark: Hashable, Codable, Identifiable {
     // MARK: - Properties
     var id: Int
     var name: String
